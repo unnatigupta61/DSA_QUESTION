cab6555a09158ff54fe90be35e448b05d5f471ec
unnatigupta61 <74136407+unnatigupta61@users.noreply.github.com>
2023-02-02 22:10:56 +0530

    Create Q11.java

---
+++ Q11.java
@@ -0,0 +1,23 @@
+Take N as input. Print all prime numbers from 2 to N.
+  import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int fact;
+		for(int i=2; i<=n; i++)
+		{
+			fact=0;
+			for(int j=2; j<=i; j++)
+			{
+				if(i%j==0)
+				{
+				fact++;
+				}
+			}	
+			if(fact==1){
+				System.out.println(i);
+			}
+		}// Your Code Here
+    }
+}
