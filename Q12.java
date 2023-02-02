69d7adb52f6e82879da63ae01de8e2813876f16f
unnatigupta61 <74136407+unnatigupta61@users.noreply.github.com>
2023-02-02 23:32:30 +0530

    Update Q12.java

--- Q12.java
+++ Q12.java
@@ -1,23 +1,20 @@
-//1-Take N as input. Print all prime numbers from 2 to N.
+//2-Take N (number in binary format). Write a function that converts it to decimal format and Print the value returned.
 import java.util.*;
 public class Main {
     public static void main(String args[]) {
-        Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		int fact;
-		for(int i=2; i<=n; i++)
-		{
-			fact=0;
-			for(int j=2; j<=i; j++)
-			{
-				if(i%j==0)
-				{
-				fact++;
-				}
-			}	
-			if(fact==1){
-				System.out.println(i);
-			}
-		}// Your Code Here
+		//int s= n.length();
+		int c=0;
+		int mul=1;
+		while(n>0){
+			int a=n%10;
+			int y= a*mul;
+			c+=y;
+			mul*=2;
+			n/=10;
+			
+		}
+		System.out.println(c);
     }
 }
