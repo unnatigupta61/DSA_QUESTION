a2649ade44d578388b3ccb62a2f9133f9b704655
unnatigupta61 <74136407+unnatigupta61@users.noreply.github.com>
2023-03-19 21:20:46 +0530

    Create q15.java

---
+++ q15.java
@@ -0,0 +1,32 @@
+//find Max and Min element in an array
+import java.util.*;
+public class MaxandMin{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Max(n,arr);
+        Min(n,arr);
+    }
+    public static void Max(int n,int[] arr){
+        int max=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+    public static void Min(int n,int[] arr){
+        int min = arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
